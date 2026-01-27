b82a498c85b11119d71a226705458b168366efb5
Flavio <flavioop@gmail.com>
2026-01-26 22:11:37 -0300

    Faça como eu fiz: mergulhando em Java

--- CarreiraJavaAlura/primeiroProjeto/src/Main.java
+++ CarreiraJavaAlura/primeiroProjeto/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println("Concluí­ a aula 01 e agora estou mergulhando em Java!");
     }
 
     //teste
