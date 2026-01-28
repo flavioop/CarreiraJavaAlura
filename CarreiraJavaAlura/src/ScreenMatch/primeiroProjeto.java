118caa084c9f3514c811d7c3dfc93a72296aa21a
Flavio <flavioop@gmail.com>
2026-01-27 21:00:42 -0300

    Screem Match Passo1

---
+++ CarreiraJavaAlura/src/ScreenMatch/primeiroProjeto.java
@@ -0,0 +1,21 @@
+package ScreenMatch;
+
+public class primeiroProjeto {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Esse é o Screen Match");
+		System.out.println("Filme: Top Gun: Maverick");
+		
+		int anoDeLancamento = 2022;
+		boolean incluidoNoPlano = true;
+		double notaFilme = 8.1;
+		
+		
+		System.out.println("Ano de lançamento: " + anoDeLancamento);
+		
+		
+
+	}
+
+}
