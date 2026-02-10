fb1a154ddf9abe605603b817ab774e60a44715f9
Flávio Pedro <flavioop@gmail.com>
2026-02-09 23:04:23 -0300

    Add files via upload

---
+++ CarreiraJavaAlura/screenmatch/src/screenmatch/Principal.java
@@ -0,0 +1,19 @@
+package screenmatch;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		Filme meuFilme = new Filme();
+		
+		meuFilme.nome = "O poderoso chefão";
+		meuFilme.anoDeLancamento = 1970;
+		meuFilme.duracaoEmMinutos = 180;
+		
+		System.out.println(meuFilme.nome);
+		System.out.println(meuFilme.anoDeLancamento);
+		
+		
+		
+	}
+
+}
